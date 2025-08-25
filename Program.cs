9800a3964ca7a56673f7dcc51b224954c75ff592
ervinhun <127263111+ervinhun@users.noreply.github.com>
2025-08-25 13:27:54 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+
+    public int MyInt { get; set; }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+    }
+}
